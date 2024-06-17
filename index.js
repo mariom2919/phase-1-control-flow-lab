ccf24da7bf91db8fc928eb4f1ae080225732bcf7
+ <+>
2024-06-16 23:18:22 -0700

    -m added index

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(distance) {
 
-function ternaryCheckCity(){
-  // Write your code here!
+  
+  if (distance <= 400) {
+    return "This one is on me!";
+  } else if (distance <= 2000) {
+    return "That will be twenty bucks.";
+  } else if (distance <= 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else {
+    return "No can do.";
+  }
 }
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function ternaryCheckCity(location) {
+  if (location === "NYC") {  
+    return "Ok, sounds good.";
+  } else {
+    return "No go.";
+  }
+}function switchOnCharmFromTip(tip) {
+  if (tip === 'generous') {  
+    return "Thank you so much.";
+  } else if (tip === 'not as generous') {  
+    return "Thank you.";
+  } else {
+    return "Bye.";
+  }
 }
